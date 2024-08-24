d40fa6dace26a1c2dd299407dc18b9055dbde109
ibaragozz <baragozz1@gmail.com>
2024-08-24 23:22:51 +0300

    pygame_template

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+window_size = 800, 600
+screen = pygame.display.set_mode((window_size))
+pygame.display.set_caption("My Test Game")
+
+
+running = True
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.fill((0, 0, 0))
+    pygame.display.flip()
+
+
+pygame.quit()
